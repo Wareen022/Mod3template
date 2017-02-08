27a5c2969a88db9606229d3947b305167501e0a8
ren bernabe <ren bernabe>
2017-02-08 16:40:27 +0800

    Edit

--- routes/addStudent.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/addStudent', function(req, res, next) {
-  res.render('addStudent');
-});
-
-module.exports = router;
